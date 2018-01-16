df3bca39d8fa4063eeaee5ed2393dd71db9bcfa5
timurridjanovic <timur.ridjanovic@gmail.com>
2018-01-16 00:13:15 -0500

    fix indentation

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const tokenizer = require('./tokenizer')
 const parser = require('./parser')
 
 const js = `
-	var books = [
-		{
-			author: 'timur',
-			price: 100
-		},
-		{
-			author: 'ogden',
-			price: 100
-		}
-	]
+  var books = [
+    {
+      author: 'timur',
+      price: 100
+    },
+    {
+      author: 'ogden',
+      price: 100
+    }
+  ]
 
-	var newBooks = books.map(function(book) {
-		return {
-			author: book.author
-		}
-	})
+  var newBooks = books.map(function(book) {
+    return {
+      author: book.author
+    }
+  })
 `
 
 const ast = parser(tokenizer(js))
